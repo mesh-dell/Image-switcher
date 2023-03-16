1d8b12ca320f337df5a50965b738db45e8a75160
mesh-dell <mwaimonicas@gmail.com>
2023-03-16 22:17:44 +0300

    Image setting

--- app/src/main/java/com/example/imageswitcher/MainActivity.kt
+++ app/src/main/java/com/example/imageswitcher/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.imageswitcher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
+lateinit var btnNext: Button
+lateinit var btnPrev: Button
+lateinit var imageView: ImageView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var i = "img"
+        var j : Int
+        btnNext = findViewById(R.id.btnNext)
+        btnPrev = findViewById(R.id.btnPrev)
+        imageView = findViewById(R.id.imageView)
+
     }
 }
